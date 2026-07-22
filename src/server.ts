1b996b76880c97174af86e7150ccca9f788ff4fc
YourGelo <160036483+YourGelo@users.noreply.github.com>
2026-07-22 18:20:46 +0300

    Добавлен базовый каркас API

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import { createApp } from "./app.js";
+import { env } from "./config/env.js";
+
+const app = createApp();
+
+app.listen(env.PORT, () => {
+  console.log(`Task Hub API запущен: http://localhost:${env.PORT}`);
+});
